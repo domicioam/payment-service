76d706508bceea94df08c2b26fc97d556b6960cc
Domício Medeiros <domicioam@gmail.com>
2021-10-24 14:32:33 +0100

    ...

--- Src/Infrastructure/Payment.Communication.RabbitMq/RabbitMqConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Payment.Communication.RabbitMq
-{
-    public class RabbitMqConfig
-    {
-        public string HostName { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
--- Src/Services/Payment.Authorise/CreditCard.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-namespace AuthorizeService
-{
-    public record CreditCard(string Number, DateTime ExpiryDate, string Cvv);
-}
