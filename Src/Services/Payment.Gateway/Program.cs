e1e7a2bdec237db62ffbccc576bd442dace49be7
Domício Medeiros <domicioam@gmail.com>
2021-10-28 07:54:49 +0100

    Add gateway project as web api

--- Src/Services/Payment.Gateway/Program.cs
+++ Src/Services/Payment.Gateway/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Payment.Gateway
 {
@@ -16,6 +18,6 @@ namespace Payment.Gateway
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) => { services.AddHostedService<Worker>(); });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
