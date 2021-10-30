2f90c73fe758f6bb29348e310ad26ccf23f307b0
Domício Medeiros <domicioam@gmail.com>
2021-10-30 08:09:05 +0100

    more tests

--- Src/Services/Payment.Capture.UnitTests/Application/CaptureApplicationServiceTests.cs
+++ Src/Services/Payment.Capture.UnitTests/Application/CaptureApplicationServiceTests.cs
@@ -26,5 +26,39 @@ namespace Payment.Capture.UnitTests.Application
             captureVerificator.Verify(c => c.CanExecuteCapture(authorisationId), Times.Once);
             mediator.Verify(m => m.Publish(It.IsAny<CaptureExecuted>(), It.IsAny<CancellationToken>()), Times.Once);
         }
+
+        [Fact]
+        public void Should_not_send_capture_event_when_cant_execute_capture()
+        {
+            var mediator = new Mock<IMediator>();
+            var captureVerificator = new Mock<CanVerifyCapture>();
+            captureVerificator.Setup(c => c.CanExecuteCapture(It.IsAny<Guid>())).Returns(false);
+            var captureApplicationService = new CaptureApplicationService(mediator.Object, captureVerificator.Object);
+            var authorisationId = Guid.NewGuid();
+            decimal amount = 10m;
+            var captureCommand = new CaptureCommand(authorisationId, amount);
+            captureApplicationService.Capture(captureCommand);
+            
+            captureVerificator.Verify(c => c.CanExecuteCapture(authorisationId), Times.Once);
+            mediator.Verify(m => m.Publish(It.IsAny<CaptureRejected>(), It.IsAny<CancellationToken>()), Times.Once);
+            mediator.Verify(m => m.Publish(It.IsAny<CaptureExecuted>(), It.IsAny<CancellationToken>()), Times.Never);
+        }
+        
+        [Fact]
+        public void Should_not_send_capture_event_when_exception_thrown()
+        {
+            var mediator = new Mock<IMediator>();
+            var captureVerificator = new Mock<CanVerifyCapture>();
+            captureVerificator.Setup(c => c.CanExecuteCapture(It.IsAny<Guid>())).Throws(new Exception());
+            var captureApplicationService = new CaptureApplicationService(mediator.Object, captureVerificator.Object);
+            var authorisationId = Guid.NewGuid();
+            decimal amount = 10m;
+            var captureCommand = new CaptureCommand(authorisationId, amount);
+            captureApplicationService.Capture(captureCommand);
+            
+            captureVerificator.Verify(c => c.CanExecuteCapture(authorisationId), Times.Once);
+            mediator.Verify(m => m.Publish(It.IsAny<CaptureRejected>(), It.IsAny<CancellationToken>()), Times.Once);
+            mediator.Verify(m => m.Publish(It.IsAny<CaptureExecuted>(), It.IsAny<CancellationToken>()), Times.Never);
+        }
     }
 }
