f113eb0889cb66251f2b78dbd2e3fb4a331e3f71
Domício Medeiros <domicioam@gmail.com>
2021-10-31 17:42:52 +0000

    listen to refund queue and invoke refund service

--- Src/Payment.Refund/Worker.cs
+++ Src/Payment.Refund/Worker.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Payment.Communication.RabbitMq;
+using Payment.EventSourcing.Messages;
+using Payment.Refund.Application;
 
 namespace Payment.Refund
 {
@@ -11,11 +14,13 @@ namespace Payment.Refund
     {
         private readonly ILogger<Worker> _logger;
         private readonly RabbitMqConsumer _rabbitMqConsumer;
+        private readonly RefundService _refundService;
 
-        public Worker(ILogger<Worker> logger, RabbitMqConsumer rabbitMqConsumer)
+        public Worker(ILogger<Worker> logger, RabbitMqConsumer rabbitMqConsumer, RefundService refundService)
         {
             _logger = logger;
             _rabbitMqConsumer = rabbitMqConsumer;
+            _refundService = refundService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -32,13 +37,20 @@ namespace Payment.Refund
         private async Task StartListeningForRefundRequests()
         {
             _rabbitMqConsumer.MessageReceived += ProcessRefundRequest;
-            await _rabbitMqConsumer.StartListeningForRequestsAsync(Queues.StoredEvents);
+            await _rabbitMqConsumer.StartListeningForRequestsAsync(Queues.Refund);
         }
 
-        private void ProcessRefundRequest(string message)
+        private async void ProcessRefundRequest(string message)
         {
-            // filter refund events
-            throw new NotImplementedException();
+            try
+            {
+                var refundCommand = JsonSerializer.Deserialize<RefundCommand>(message);
+                await _refundService.Refund(refundCommand);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error while processing message.", message);
+            }
         }
     }
 }
