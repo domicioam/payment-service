7e0be8110fdb2262e09633a83e0adcc2abe7f96b
Domício Medeiros <domicioam@gmail.com>
2021-11-13 20:20:17 +0000

    dummy solution for connection retry

--- Src/Payment.Communication.RabbitMq/RabbitMqConsumer.cs
+++ Src/Payment.Communication.RabbitMq/RabbitMqConsumer.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
@@ -8,12 +10,14 @@ using RabbitMQ.Client.Events;
 namespace Payment.Communication.RabbitMq
 {
     public delegate void DoWork(string message);
+
     public class RabbitMqConsumer
     {
         private readonly ILogger<RabbitMqConsumer> _logger;
         private IConnection _connection;
         private IModel _channel;
         private readonly RabbitMqConfig _rabbitMqConfig;
+        private Timer timer;
 
         public event DoWork MessageReceived;
 
@@ -21,38 +25,54 @@ namespace Payment.Communication.RabbitMq
         {
             return Task.Run(() =>
             {
-                var factory = new ConnectionFactory()
+                timer = new Timer(3000);
+                timer.Elapsed += (sender, args) =>
                 {
-                    HostName = _rabbitMqConfig.HostName,
-                    UserName = _rabbitMqConfig.UserName,
-                    Password = _rabbitMqConfig.Password
+                    try
+                    {
+                        var factory = new ConnectionFactory()
+                        {
+                            HostName = _rabbitMqConfig.HostName,
+                            UserName = _rabbitMqConfig.UserName,
+                            Password = _rabbitMqConfig.Password
+                        };
+
+                        _connection = factory.CreateConnection();
+                        _channel = _connection.CreateModel();
+                        _channel.QueueDeclare(queue: queueName, durable: true, exclusive: false,
+                            autoDelete: false, arguments: null);
+
+                        _channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                        var consumer = new EventingBasicConsumer(_channel);
+                        consumer.Received += (sender, e) =>
+                        {
+                            var body = e.Body;
+                            var message = Encoding.UTF8.GetString(body.Span);
+
+                            MessageReceived(message);
+
+                            _channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: true);
+                        };
+
+                        consumer.Registered += (sender, args) =>
+                        {
+                            _logger.LogInformation("Consumer registered.");
+                            timer.Stop();
+                        };
+
+                        consumer.Unregistered += (sender, args) => { _logger.LogWarning("Consumer unregistered."); };
+                        consumer.ConsumerCancelled += (sender, args) => { _logger.LogWarning("Consumer cancelled."); };
+
+                        _channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogCritical("Error when trying to connect to queue.", e);
+                    }
                 };
-
-                _connection = factory.CreateConnection();
-                _channel = _connection.CreateModel();
-                _channel.QueueDeclare(queue: queueName, durable: true, exclusive: false,
-                                        autoDelete: false, arguments: null);
-
-                _channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
-
-                var consumer = new EventingBasicConsumer(_channel);
-                consumer.Received += (sender, e) =>
-                {
-                    var body = e.Body;
-                    var message = Encoding.UTF8.GetString(body.Span);
-
-                    MessageReceived(message);
-
-                    _channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: true);
-                };
-                
-                consumer.Registered += (sender, args) => { _logger.LogInformation("Consumer registered."); };
-
-                consumer.Unregistered += (sender, args) => { _logger.LogWarning("Consumer unregistered."); };
-
-                consumer.ConsumerCancelled += (sender, args) => { _logger.LogWarning("Consumer cancelled."); };
-
-                _channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+                timer.Enabled = true;
+                timer.AutoReset = true;
             });
         }
 
@@ -84,7 +104,6 @@ namespace Payment.Communication.RabbitMq
         // Necessary for mocking
         protected RabbitMqConsumer() : base()
         {
-
         }
     }
 }
